25a735c588ab482bb51de4b63e548ce83cbd1ac1
KYRIE KANE <146706868+meardagoat@users.noreply.github.com>
2023-10-03 16:06:09 +0200

    update

---
+++ day_03/ex_05.js
@@ -0,0 +1,7 @@
+function getBasketContent() {
+    let fruits = ["strawberry", "strawberry","strawberry", "apple", "apple", "lime", "lime", "peach","pear", "pear"]
+
+    console.log ("10 fruit(s) selected")
+    console.log(fruits.legnth);
+    return fruits
+}
