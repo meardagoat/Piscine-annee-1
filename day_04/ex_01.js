f3443653d6f8e528d7e97cab3e4c764d9bc5b7a8
KYRIE KANE <146706868+meardagoat@users.noreply.github.com>
2023-10-04 14:48:58 +0200

    update day 4

---
+++ day_04/ex_01.js
@@ -0,0 +1,8 @@
+function getAngryDog(numberOfWoofs) {
+    // Your code here
+    let result = ""
+    for (let i = 0; i < numberOfWoofs; i++) {
+        result = result + "woof";
+    }
+    return result
+}
