b6f9563681cb795f8107c98ac37639c1c0881fc7
KYRIE KANE <146706868+meardagoat@users.noreply.github.com>
2023-10-12 16:07:40 +0200

    update yeah yeah

---
+++ day_09/ex_06/ex_06.js
@@ -0,0 +1,21 @@
+function particlesJS(particlesJs, param2) {
+
+}
+
+particlesJS('particles-js', {
+    canvas: {
+        color_hex_bg: '#15414e',
+        opacity: 1
+    },
+    particles: {
+        color_hex: '#fff',
+        opacity: 1,
+        size: 2.5,
+        size_random: true,
+        nb: 200,
+        anim: {
+            speed: 2
+        }
+    },
+    retina_detect: true
+});
